7e213e5bcff1a3a79e360ff092f54964bcc82ce3
Kushal Hada <kus@tilde.team>
2021-05-16 17:09:02 -0600

    throw an error
    to test sentry

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,14 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   title = 'Angular 12';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.sayHello();
   }
 
+  sayHello() {
+    throw new Error('Function not implemented.');
+  }
 }
