bad2c9c144d2f2d0f796166de9cb29184eed708a
Kushal Hada <kus@tilde.team>
2021-05-16 17:32:18 -0600

    update angular

--- README.md
+++ README.md
@@ -163,3 +163,4 @@ TOTAL: 1 FAILED, 2 SUCCESS
 info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
 ```
 Sun May 16 05:30:45 PM MDT 2021
+Sun May 16 05:32:18 PM MDT 2021
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +9,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        NavigationBarComponent,
+        HomeComponent ]
     })
     .compileComponents();
   });
